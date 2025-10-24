a572069d2e03b55f92e642c6057d7e35c72ca507
RAVI RANJAN KUMAR <2507raviranjan@gmail.com>
2025-10-24 17:01:08 +0530

    Update introduction.c

--- LAB1/introduction.c
+++ LAB1/introduction.c
@@ -8,7 +8,7 @@ int main() {
     printf("Dept   : Mechanical+MBA\n");
     printf("Roll No  : MEBM25005\n");
     printf("INSTITUTE   : TEZPUR UNIVERSITY\n");
-    printf("Hobbies  : Reading, Coding, and Playing Football\n");
+    printf("Hobbies  : Reading novels,Watching Web Series, Playing Cricket\n");
 
     return 0;
 }
