6a9a643356a5799f33e90391e004575b1515caf9
RAVI RANJAN KUMAR <2507raviranjan@gmail.com>
2025-10-24 16:39:53 +0530

    Update temparature_check_analysis.c

--- LAB3/temparature_check_analysis.c
+++ LAB3/temparature_check_analysis.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+
+int main() {
+    float temp;
+    printf("Enter current temperature (°C): ");
+    scanf("%f", &temp);
+
+    if (temp > 60 && temp <= 82)
+        printf("Temperature is safe.\n");
+    else
+        printf("Temperature warning!\n");
+
+    return 0;
+}
+
